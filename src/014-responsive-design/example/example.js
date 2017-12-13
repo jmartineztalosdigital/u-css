490ba61ac119267ebfbf364d05fbb9f8ef0d57a3
Jesus martinez sanchez <jesusdavid.martinez@talosdigital.com>
2017-12-13 16:41:52 -0500

    014 practice

---
+++ src/014-responsive-design/example/example.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $("#toggle-menu-icon").click(function(){
+    $(".toggle-menu").show();
+  });
+
+  $("#close-toggle-menu").click(function(){
+    $(".toggle-menu").hide();
+  });
+
+
+});
